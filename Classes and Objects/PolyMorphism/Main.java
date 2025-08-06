7cebba3c7943a1f5b5a75899b3594d88220d98fa
Harsh Sehrawat <harsehrawat@gmail.com>
2025-08-06 18:23:41 +0530

    updation

--- Classes and Objects/PolyMorphism/Main.java
+++ Classes and Objects/PolyMorphism/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Vet v = new Vet();
         Dog d = new Dog();
         Animal h = new Hippo();
-
+        Animal a = new Animal(); // why will it not compile?
         v.giveShot(d);   
         v.giveShot(h);
     }
