3760eb7f889a847def0ac5a9ca7899c55713bccf
Harsh Sehrawat <harsehrawat@gmail.com>
2025-08-13 12:03:03 +0000

    updated

--- Classes and Objects/PolyMorphism/Main.java
+++ Classes and Objects/PolyMorphism/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Vet v = new Vet();
         Dog d = new Dog();
         Animal h = new Hippo();
-        Animal a = new Animal(); // why will it not compile?
+        Animal a = new Animal(); // it will compile but think, do we need to allow the user to have an object of Animal type itself? HINT: Interfaces.
         v.giveShot(d);   
         v.giveShot(h);
     }
